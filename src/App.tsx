4b1d947e9c254dcd4be6f9c724d1906f63921d95
1084350607 <5550076+tsy210258@user.noreply.gitee.com>
2021-03-14 19:35:59 +0800

    添加src

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Home from './pages/home'
+// 全局导入antd
+import 'antd/dist/antd.less'
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from 'antd';
+
+function Home(){
+  return(
+    <div>
+      Home测试
+      <Button type="primary">antd样式测试</Button>
+    </div>
+  )
+}
+
+export default Home
